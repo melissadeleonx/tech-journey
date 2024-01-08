7b67f0bea706a0cc5738240737287f79cc931e98
melissadeleonx <primavita19@gmail.com>
2024-01-08 13:08:47 +0100

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['user-images.githubusercontent.com'],
+//   },
+//   experimental: {
+//     appDir: false,
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
@@ -7,4 +18,19 @@ module.exports = {
   experimental: {
     appDir: false,
   },
+  async headers() {
+    return [
+      {
+        // Set Cache-Control header for static assets (images, etc.)
+        source: '/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600', // Set the duration in seconds (1 hour in this example)
+          },
+        ],
+      },
+    ];
+  },
 };
+
